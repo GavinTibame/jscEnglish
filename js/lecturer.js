a5e99758218ad38fea76e4338d8fc6190106981d
GavinTibame <tibamehui@gmail.com>
2022-11-16 17:55:05 +0800

    update some

--- js/lecturer.js
+++ js/lecturer.js
@@ -1,21 +1,258 @@
 const lecturerList = [
     {
-        id: "1",
-        name: "沈維哲",
-        subject: ["英文學科"],
-        img: "img/teacher/psed/amy-hirschi-b3AYk8HKCl0-unsplash.png"
+        id: 1,
+        img: "img/teacher/psed/amy-hirschi-b3AYk8HKCl0-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "沈維哲"
     },
-
-], lecturerItem = document.querySelectorAll(".list div.inner"),
+    {
+        id: 2,
+        img: "img/teacher/psed/annika-palmari-RIt88XBR3G0-unsplash.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "宋可其"
+    },
+    {
+        id: 3,
+        img: "img/teacher/psed/austin-distel-7uoMmzPd2JA-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "蔡穎香"
+    },
+    {
+        id: 4,
+        img: "img/teacher/psed/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "姜卉云"
+    },
+    {
+        id: 5,
+        img: "img/teacher/psed/christina-wocintechchat-com-50TkCaP8M3A-unsplash.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "陳建霖"
+    },
+    {
+        id: 6,
+        img: "img/teacher/psed/christina-wocintechchat-com-kXfBDl0fR1E-unsplash.png",
+        subject: [
+            "物理學科"
+        ],
+        name: "鞏曉雯"
+    },
+    {
+        id: 7,
+        img: "img/teacher/psed/christina-wocintechchat-com-PlikkWB79qs-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "甘盈君"
+    },
+    {
+        id: 8,
+        img: "img/teacher/psed/christina-wocintechchat-com-Zpzf7TLj_gA-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "林聖仁"
+    },
+    {
+        id: 9,
+        img: "img/teacher/psed/good-faces-knoww5xIlWc-unsplash.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "楊雅俐"
+    },
+    {
+        id: 10,
+        img: "img/teacher/psed/itay-verchik-YmQ8TrsieE4-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "葉亦迪"
+    },
+    {
+        id: 11,
+        img: "img/teacher/psed/juan-encalada-WC7KIHo13Fc-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "吳左茹"
+    },
+    {
+        id: 12,
+        img: "img/teacher/psed/jurica-koletic-7YVZYZeITc8-unsplash.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "王耀合"
+    },
+    {
+        id: 13,
+        img: "img/teacher/psed/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.png",
+        subject: [
+            "物理學科"
+        ],
+        name: "傅正雅"
+    },
+    {
+        id: 14,
+        img: "img/teacher/psed/linkedin-sales-solutions-QgYvORVDdd8-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "韓軒豪"
+    },
+    {
+        id: 15,
+        img: "img/teacher/psed/michael-dam-mEZ3PoFGs_k-unsplash.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "樂俊彥"
+    },
+    {
+        id: 16,
+        img: "img/teacher/psed/omar-yehia-toCKeKuQpL0-unsplash.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "張隆坤"
+    },
+    {
+        id: 17,
+        img: "img/teacher/psed/pexels-andrea-piacquadio-3756679.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "傅旭堅"
+    },
+    {
+        id: 18,
+        img: "img/teacher/psed/pexels-andrea-piacquadio-3771089.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "張苑利"
+    },
+    {
+        id: 19,
+        img: "img/teacher/psed/pexels-andrea-piacquadio-3785104.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "楊政峰"
+    },
+    {
+        id: 20,
+        img: "img/teacher/psed/pexels-andrea-piacquadio-3824771.png",
+        subject: [
+            "物理學科"
+        ],
+        name: "陳順宜"
+    },
+    {
+        id: 21,
+        img: "img/teacher/psed/pexels-christina-morillo-1181695.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "周忠翰"
+    },
+    {
+        id: 22,
+        img: "img/teacher/psed/pexels-cottonbro-4098274.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "許雅名"
+    },
+    {
+        id: 23,
+        img: "img/teacher/psed/pexels-dellon-thomas-3394347.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "蔡秉岑"
+    },
+    {
+        id: 24,
+        img: "img/teacher/psed/pexels-dinielle-de-veyra-4195342.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "許智年"
+    },
+    {
+        id: 25,
+        img: "img/teacher/psed/pexels-dziana-hasanbekava-5480687.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "郭夢燦"
+    },
+    {
+        id: 26,
+        img: "img/teacher/psed/pexels-emmy-e-2381069.png",
+        subject: [
+            "理化學科"
+        ],
+        name: "陳珮喜"
+    },
+    {
+        id: 27,
+        img: "img/teacher/psed/pexels-karolina-grabowska-4467687.png",
+        subject: [
+            "物理學科"
+        ],
+        name: "柳東霞"
+    },
+    {
+        id: 28,
+        img: "img/teacher/psed/pexels-mentatdgt-936564.png",
+        subject: [
+            "英文學科"
+        ],
+        name: "朱財水"
+    }
+]
+    , lecturerItem = document.querySelectorAll(".list div.inner"),
     text = [];
+let tid = 1;
 lecturerItem.forEach(div => {
-    console.log(div.children[0].src.replace("http://127.0.0.1:5501/", ""));
-    console.log(div.children[1].children[0].innerText);
-    console.log(div.children[1].children[1].innerText);
-    text.push(div.children[0].src.replace("http://127.0.0.1:5501/", ""));
-    text.push(div.children[1].children[0].innerText);
-    text.push(div.children[1].children[1].innerText);
-    var data = new Blob([text], { type: 'text/plain' });
-    textFile = window.URL.createObjectURL(data);
-    return textFile;
-})
+    const data = {};
+    data.id = tid;
+    data.img = div.children[0].src.replace("http://127.0.0.1:5501/", "");
+    data.subject = [div.children[1].children[0].innerText];
+    data.name = div.children[1].children[1].innerText;
+    text.push(data);
+    tid++;
+});
+function createLecturerCardElement(object) {
+    const htmlElement = `<a id="lecturer${object.id}"href="#">
+<div class="inner">
+    <img src="${object.img}" alt="">
+    <div class="text">
+        <p>${object.subject}</p>
+        <span>${object.name}</span>
+    </div>
+</div>
+</a>`;
+    return htmlElement;
+}
+
+function renderLecturerList() {
+    lecturerList.forEach(lecturer => {
+        const card = createLecturerCardElement(lecturer);
+        $("#lecturerList").append(card);
+    })
+};
+renderLecturerList()
